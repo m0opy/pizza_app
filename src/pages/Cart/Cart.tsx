0401007dd1a29918a39b58287d2d43f1a28d750e
m0opy <schetkinapolina@gmail.com>
2024-07-14 11:36:02 +0300

    Fix

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,11 +1,9 @@
-import MenuSearch from '../../components/MenuSearch/MenuSearch';
 import Title from '../../components/Title/Title';
 
 export function Cart() {
     return (
         <>
             <Title>Корзина</Title>
-            <MenuSearch />
         </>
     );
 }
