ce48333852b8d298ffb3bb5b06a7e7fbf06aec46
m0opy <schetkinapolina@gmail.com>
2024-07-14 11:30:34 +0300

    Test pull request

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,5 +1,11 @@
+import MenuSearch from '../../components/MenuSearch/MenuSearch';
 import Title from '../../components/Title/Title';
 
 export function Cart() {
-    return <Title>Корзина</Title>;
+    return (
+        <>
+            <Title>Корзина</Title>
+            <MenuSearch />
+        </>
+    );
 }
